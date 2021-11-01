04cbffaec0c5364ee838656655c80097c71b8a84
Julia Zharova <julia.amp.zharova@gmail.com>
2021-11-01 20:12:28 +0200

    Implement home work 1

--- HW1/src/com/company/Main.java
+++ HW1/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String name;
+	    String address;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        name = scanner.nextLine();
+
+        System.out.println("Where are you live, " + name + "?");
+        address = scanner.nextLine();
+
+        System.out.println("Name: " + name + "\nAddress: " + address);
     }
 }
